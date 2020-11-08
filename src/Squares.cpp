1cb9729c1d27fb867d91c3edd593eda09999152b
Thomas Bui <thomasthienbui@gmail.com>
2020-11-08 10:21:16 +0700

    Set up SDL2 and move all cmake files into one root cmake file

--- src/Squares.cpp
+++ src/Squares.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Squares" << std::endl;
+#include <SDL.h>
+
+int main(int, char**) {
+    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cout << "SDL init error!!!" << std::endl;
+        return 1;
+    }
+    std::cout << "SDL init okay!" << std::endl;
+    SDL_Quit();
     return 0;
 }
